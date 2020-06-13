36e8e32a0691e9bcfcb727ba28716a49397999c2
fortuna8.2 <fortuna8228@gmail.com>
2020-06-14 00:04:07 +0300

    сделал адаптив для 940рх

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,17 @@ $('.icon-search').click(function (e) {
 $('.mini-menu').click(function () {
 	$('.menu ul').toggleClass('show');
 });
+
+
+// ------слайдер -----
+
+$(document).ready(function () {
+	$('.slider').slick({
+		autoplay: true,
+		autoplaySpeed: 1500,
+		arrows: true,
+		dots: false,
+		prevArrow: "<div class='prev'><img src='/../img/left-arrow.png'></div>",
+		nextArrow: "<div class='next'><img src='../img/right-arrow.png'></div>" // без div не работает
+	});
+});
