bc3cdc110e6a24ddbc4d3eeebf569df21daacf5a
fortuna8.2 <fortuna8228@gmail.com>
2020-06-13 12:13:07 +0300

    начал карусель

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$('.icon-search').click(function (e) {
+	$('.input-search, .icon-search').toggleClass('show-input');
+});
+
+$('.mini-menu').click(function () {
+	$('.menu ul').toggleClass('show');
+});
