c223f096d8f939a1d1deb321271f69ed4c953165
fortuna8.2 <fortuna8228@gmail.com>
2020-06-14 17:00:43 +0300

    поменял стили для стрелок слайдера

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,22 @@ $('.mini-menu').click(function () {
 
 
 // ------слайдер -----
+// поменял стили внутри slick-theme.css
 
 $(document).ready(function () {
 	$('.slider').slick({
-		autoplay: true,
+		// autoplay: true,
 		autoplaySpeed: 1500,
 		arrows: true,
 		dots: false,
-		prevArrow: "<div class='prev'><img src='/../img/left-arrow.png'></div>",
-		nextArrow: "<div class='next'><img src='../img/right-arrow.png'></div>" // без div не работает
+		// prevArrow: "<div class='prev'><img src='/../img/left-arrow.png'></div>",
+		// nextArrow: "<div class='next'><img src='../img/right-arrow.png'></div>" // без div не работает
+
+
+
+		// ----------а этот код ниже почему то не работает-----------------
+		// prevArrow: $(".next "),
+		// nextArrow: $(".prev") // без div не работает
+
 	});
 });
